c4b421a417005a88134238aa2f284be4f7932ff2
Travis Little <kingtrav@gmail.com>
2019-08-13 19:25:38 -0600

    mvp met

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
-const express = 'express';
+const express = require('express')
+
+const UserRouter = require('./users/userRouter.js')
 
 const server = express();
+
+const bodyParser = express.json()
+
+server.use(express.json())
+
+server.use('/api/users', UserRouter)
+
+server.use(bodyParser)
+server.use(logger)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
@@ -9,7 +20,9 @@ server.get('/', (req, res) => {
 //custom middleware
 
 function logger(req, res, next) {
-
+  const datetime = new Date()  
+  console.log(`${datetime.toISOString()} REQUEST: ${req.method}`)
+  next()
 };
 
 module.exports = server;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,125 @@
-const express = 'express';
+const express = require('express')
+const Users = require('./userDb')
+const Posts = require('../posts/postDb')
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
 
+
+router.post('/', validateUser, async (req, res) => {
+    try {
+        const post = await Users.insert(req.body)
+        res.status(201).json(post)
+    } catch (error) {
+        res.status(500).json({ message: 'There was an error with the server database.'})
+    }
 });
 
-router.post('/:id/posts', (req, res) => {
-
+router.post('/:id/posts', validatePost, async (req, res) => {
+    try {
+        const body = req.body
+        const newPost = await Posts.insert(body)
+        res.status(201).json(newPost)
+    } catch (error) {
+        res.status(500).json({ message: 'There was an error with the server database.'})
+    }
 });
 
-router.get('/', (req, res) => {
-
+router.get('/', async (req, res) => {
+    try {
+        const users = await Users.get()
+        res.status(200).json(users)
+    } catch (error) {
+        res.status(500).json({ message: 'There was an error with the server database.'})
+    }
 });
 
-router.get('/:id', (req, res) => {
-
+router.get('/:id', validateUserId, async (req, res) => {
+    try {
+        const { id } = req.params
+        const user = await Users.getById(id)
+        res.status(200).json(user)        
+    } catch (error) {
+        res.status(500).json({ message: 'There was an error with the server database.'})
+    }
 });
 
-router.get('/:id/posts', (req, res) => {
-
+router.get('/:id/posts', validateUserId, async (req, res) => {
+    try {
+        const { id } = req.params
+        const posts = await Users.getUserPosts(id)
+        res.status(200).json(posts)
+    } catch (error) {
+        res.status(500).json({ message: 'There was an error with the server database.'})
+    }
 });
 
-router.delete('/:id', (req, res) => {
-
+router.delete('/:id', validateUserId, async (req, res) => {
+    try {
+        const id = req.user
+        const deletedUser = await Users.remove(id)
+        res.status(200).json({ message: `ID: ${id} was removed, ${deletedUser} user(s) deleted` })
+    } catch (error) {
+        res.status(500).json({ message: 'There was an error with the server database.'})
+    }
 });
 
-router.put('/:id', (req, res) => {
-
+router.put('/:id', validateUserId, async (req, res) => {
+    try {
+        const { id } = req.params
+        const { body } = req
+        const updateUser = await Users.update(id, body)
+        res.status(200).json({ message: `${updateUser} user(s) updated` })
+    } catch (error) {
+        res.status(500).json({ message: 'There was an error with the server database.'})
+    }
 });
 
 //custom middleware
 
-function validateUserId(req, res, next) {
-
+async function validateUserId(req, res, next) {
+    try {
+        const { id } = req.params
+        const user = await Users.getById(id)
+        if (!user) {
+            throw {
+                code: 400,
+                message: 'invalid user id'
+            }
+        }
+        req.user = id
+        next()
+    } catch (error) {
+        if(!error.code) {
+            res.status(500).json({ message: 'There was an error with the server database.'})
+        }
+        res.status(error.code).json({
+            message: error.message
+        })
+    }
+    
 };
 
 function validateUser(req, res, next) {
-
+    const body = req.body
+    if (Object.keys(body).length < 1) {
+        res.status(400).json({ message: 'missing user data'})
+    }
+    if (!body.name) {
+        res.status(400).json({ message: `missing required name field` })
+    }
+    next()
 };
 
 function validatePost(req, res, next) {
-
+    const body = req.body
+    if (Object.keys(body).length < 1) {
+        res.status(400).json({ message: 'missing post data'})
+    }
+    if (!body.text || !body.user_id) {
+        res.status(400).json({ message: `missing required text or userid field` })
+    }
+    next()
 };
 
 module.exports = router;
